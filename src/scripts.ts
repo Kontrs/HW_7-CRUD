5757fae742035bb67fbc17943a781bfa91371140
Kristers Pulle <pulle265@gmail.com>
2023-11-23 21:12:30 +0200

    hw done

--- src/scripts.ts
+++ src/scripts.ts
@@ -11,7 +11,6 @@ type Game = {
     creationTime: Date
 }
 const cardWrapper = document.querySelector<HTMLDivElement>('.js-card-wrapper');
-// const cardEditButton = document.querySelectorAll<HTMLButtonElement>('.js-edit-button');
 
 // Function for card creation time (created 'X time ago')
 
@@ -149,25 +148,36 @@ cardWrapper.addEventListener('click', (event) => {
     if (gameCard) {
       const gameId = Number(gameCard.dataset.gameId || '');
       const originalCardImage = (gameCard.querySelector<HTMLImageElement>('.card__image').src || '/assets/images/default-image.jpg');
-      gameCard.querySelectorAll('.card input, .card textarea').forEach((input) => { // fixing
-        console.log(input);
-      });
 
       gameCard.innerHTML = `
         <img class='card__image' src='${originalCardImage}' alt='game-img'>
-        <input class='card__title' id='title' value='${gameCard.querySelector('.card__title').textContent}'>
-        <input class='card__genre' id='genre' value='${gameCard.querySelector('.card__genre').textContent}'>
-        <textarea class='card__description' id='description'>${gameCard.querySelector<HTMLTextAreaElement>('.card__description').textContent}</textarea>
+        <input class='card__title edit-mode' id='title-edit' value='${gameCard.querySelector('.card__title').textContent}'>
+        <input class='card__genre edit-mode' id='genre-edit' value='${gameCard.querySelector('.card__genre').textContent}'>
+        <textarea class='card__description edit-mode' id='description-edit'>${gameCard.querySelector<HTMLTextAreaElement>('.card__description').textContent}</textarea>
+        <div class="input__rating js-input-rating">
+                <input type="radio" id="star5" name="rating" value="5" class="js-star-rating"/>
+                <label class="star" for="star5" title="Awesome" aria-hidden="true"></label>
+                <input type="radio" id="star4" name="rating" value="4" class="js-star-rating"/>
+                <label class="star" for="star4" title="Great" aria-hidden="true"></label>
+                <input type="radio" id="star3" name="rating" value="3" class="js-star-rating"/>
+                <label class="star" for="star3" title="Very good" aria-hidden="true"></label>
+                <input type="radio" id="star2" name="rating" value="2" class="js-star-rating"/>
+                <label class="star" for="star2" title="Good" aria-hidden="true"></label>
+                <input type="radio" id="star1" name="rating" value="1" class="js-star-rating"/>
+                <label class="star" for="star1" title="Bad" aria-hidden="true"></label>
+            </div>
         <div class="card__buttons">
           <button class="buttons__save js-save-button">Save</button>
         </div>
       `;
       const saveButton = gameCard.querySelector<HTMLButtonElement>('.js-save-button');
       saveButton.addEventListener('click', () => {
+        const selectedStar = gameCard.querySelector<HTMLInputElement>('.js-star-rating:checked');
         const updatedData = {
           title: (gameCard.querySelector<HTMLElement>('.card__title') as HTMLInputElement).value,
           genre: (gameCard.querySelector<HTMLElement>('.card__genre') as HTMLInputElement).value,
           description: (gameCard.querySelector<HTMLElement>('.card__description') as HTMLTextAreaElement).value,
+          rating: selectedStar.value,
         };
         editGameCard(gameId, updatedData);
       });
